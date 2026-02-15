43cd687030e0e242e3340a3992e717892f3441b1
Zoriot <zoriot@proton.me>
2026-02-15 17:52:31 +0100

    build: Fix publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,23 +49,10 @@ tasks.shadowJar {
     enableAutoRelocation = true
 }
 
-tasks.assemble {
-    dependsOn(tasks.shadowJar) // Ensure that the shadowJar task runs before the build task
-}
-
-tasks.jar {
-    archiveClassifier = "UNSHADED"
-    enabled = false // Disable the default jar task since we are using shadowJar
-}
-
 publishing {
-    publications.create<MavenPublication>("maven") {
+    publications.create<MavenPublication>("mavenJava") {
         from(components["java"])
     }
-}
-
-tasks.withType<GenerateModuleMetadata> {
-    enabled = false
 }
 
 
